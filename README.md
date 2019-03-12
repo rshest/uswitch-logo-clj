9e82c952c4083e283177c52a9b21a1bc1afb529c
Ruslan Shestopalyuk <rushbits@gmail.com>
2019-03-12 01:07:07 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# uSwitch logo drawn procedurally via Clojure
+
+## About
+This is a little excercise done for fun during a [Clojure Dojo London meetup](https://www.meetup.com/London-Clojurians/events/wndklqyzfbpb/) at uSwitch office.
+
+The goal was to use a library called [clisk](https://github.com/mikera/clisk) for doing something visually nice.
+
+This repo is the result, it renders the uSwitch logo in a procedural way using the Clisk DSL.
+
+Clean logo:
+
+![](img/uswitch-logo-clean.png)
+
+A bit of distortion to demontrate combination with Clisk base operators:
+
+![](img/uswitch-logo-distorted.png)
+
+The logo is made from primitive shapes (circles and rectangles), which are correspondingly scaled/offset and then combined via boolean operations.
+
+This is further coloured and distorted.
+
+## Running
+
+```bash
+lein run
+```
---
+++ src/uswitch_logo_clj/core.clj
@@ -0,0 +1,64 @@
+(ns uswitch-logo-clj.core
+  (:require [clisk.live :refer :all]))
+
+(def ^:const uswitch-blue [0.007 0.01 0.61])
+
+; shape boolean operations
+(defn intersection ([& args] (clamp (apply vmin args) [0 0 0] [1 1 1])))
+(defn union ([& args] (clamp (apply vmax args) [0 0 0] [1 1 1])))
+(defn difference ([& args] (clamp (apply v- args) [0 0 0] [1 1 1])))
+
+; circle equation
+(defn circle ([r]
+  (vif `(- (+ (* ~'y ~'y) (* ~'x ~'x)) (* ~r ~r)) 
+        black
+        white)))
+
+; rectrangle equation, see http://mathforum.org/library/drmath/view/77216.html
+(defn rect ([w h]
+  (vif `(+ (Math/abs (+ (/ ~'x ~w) (/ ~'y ~h))) 
+           (Math/abs (- (/ ~'x ~w) (/ ~'y ~h))) -2) 
+        black
+        white)))
+
+; all pixels below y plane, used for cutting out
+(defn lower-half-plane ([] (if (> y 0) black white)))
+
+; triangle equation (right-angled, pointed upwards)
+; made by rotating a rectangle and subtracting a rectangle from the bottom part
+(defn triangle ([s]
+  (difference
+    (rotate (/ Math/PI 4) (rect s s))
+    (offset [0 (- s)] (rect (* 2 s) s)))))
+
+; a "letter u" equation
+(defn letter-u ([w]
+  (difference
+    (union 
+      (circle 0.3)
+      (offset [0 0.12] (rect 0.3 0.15)))
+    (circle (- 0.3 w))
+    (offset [0 0.3] (rect (- 0.3 w) 0.3)))))
+
+; combined shape of the logo
+(defn uswitch-logo-shape ([]
+  (union
+    (difference
+      (letter-u 0.15)
+      (offset [-0.15 0.05] (letter-u 0.06)))
+    (offset [-0.22 0.22] (triangle 0.09)))))
+
+;(show (offset [-0.5 -0.5] (letter-u 0.15)))
+
+(defn uswitch-logo ([]
+  (v+ uswitch-blue (offset [-0.5 -0.5] (uswitch-logo-shape)))))
+
+(defn offset-noise ([]
+  (v* (seamless 0.1 (compose plasma vsnoise)) [0.05 0.05 0.05])))
+
+(defn -main [& args]
+  (do
+    (show (offset (offset-noise) (uswitch-logo)))
+    ; uncomment to show the clean logo
+    ;(show (uswitch-logo))
+  ))
